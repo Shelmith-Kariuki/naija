42abfcb4308439e1eb122148e4fa154a82f1da61
Shelmith-Kariuki <kariukishelmith@gmail.com>
2020-08-15 13:35:22 +0300

    Initial commit

---
+++ R/care_message.R
@@ -0,0 +1,13 @@
+#' Bauchi package development training
+#'
+#' @param name The name of the message receiver.
+#'
+#' @return message
+#' @export
+#'
+#' @examples
+#' care_message("Hadley")
+care_message <- function(name){
+  paste("Hi", name, ", we hope you are taking care of yourself.")
+}
+
